91ebce10b4a9cf6fdb7a1b1ee9ece5f0429de0c3
smpledvid <dmoney.liang@gmail.com>
2021-01-28 20:44:33 -0800

    Added avatar into contact section.

--- src/components/Contact/contact.component.js
+++ src/components/Contact/contact.component.js
@@ -1,6 +1,8 @@
 import { React, useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import CorgiImage from '../../assets/images/CORGI.jpg';
 
 import './contact.scss';
 
@@ -17,6 +19,11 @@ const useStyles = makeStyles((theme) => ({
     input: {
         fontSize: '1.5em',
         padding: '0.5em'
+    },
+    avatar: {
+        width: '15em',
+        height: '15em',
+        marginTop: '3em'
     }
 }));
 
@@ -31,57 +38,64 @@ function Contact() {
             <div className="row">
                 <div className="col-md-12 section-titles">Contact Me</div>
             </div>
-            <div className="row">
-                <div className="col-md-6">
-                    <div className="input-labels">Name:</div>
-                    <div>
-                        <TextField 
-                            variant="outlined" 
-                            className={classes.messageInput}
-                            InputProps={{
-                                classes: {
-                                  input: classes.input,
-                                },
-                            }}
-                            onChange={(e) => setName(e.target.value)}
-                            value={name}
-                        />
-                    </div>
+            <div className="row contact-wrapper">
+                <div className="col-md-6 avatar-wrapper">
+                    <Avatar alt="Remy Sharp" src={CorgiImage} className={classes.avatar}/>
                 </div>
                 <div className="col-md-6">
-                    <div className="input-labels">Email:</div>
-                    <div>
-                        <TextField 
-                            variant="outlined" 
-                            className={classes.messageInput}
-                            InputProps={{
-                                classes: {
-                                  input: classes.input,
-                                },
-                            }}
-                            onChange={(e) => setEmail(e.target.value)}
-                            value={email}
-                        />
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="input-labels">Name:</div>
+                            <div>
+                                <TextField 
+                                    variant="outlined" 
+                                    className={classes.messageInput}
+                                    InputProps={{
+                                        classes: {
+                                        input: classes.input,
+                                        },
+                                    }}
+                                    onChange={(e) => setName(e.target.value)}
+                                    value={name}
+                                />
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="input-labels">Email:</div>
+                            <div>
+                                <TextField 
+                                    variant="outlined" 
+                                    className={classes.messageInput}
+                                    InputProps={{
+                                        classes: {
+                                        input: classes.input,
+                                        },
+                                    }}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    value={email}
+                                />
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-md-12">
-                    <div className="input-labels">Message:</div>
-                    <div>
-                        <TextField 
-                            variant="outlined" 
-                            className={classes.messageInput}
-                            multiline
-                            rows={10}
-                            InputProps={{
-                                classes: {
-                                  input: classes.input,
-                                },
-                            }}
-                            onChange={(e) => setMessage(e.target.value)}
-                            value={message}
-                        />
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="input-labels">Message:</div>
+                            <div>
+                                <TextField 
+                                    variant="outlined" 
+                                    className={classes.messageInput}
+                                    multiline
+                                    rows={10}
+                                    InputProps={{
+                                        classes: {
+                                        input: classes.input,
+                                        },
+                                    }}
+                                    onChange={(e) => setMessage(e.target.value)}
+                                    value={message}
+                                />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
